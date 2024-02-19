83541823f1a078692c7c9b4eef58a67c0237b5bb
root <binbin.he515@outlook.com>
2024-02-19 13:03:55 +0000

    tsk4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle.\n" 
+ * main - Prints "Programming is like building a multilingual puzzle." 
  * Return: Always 0.
  */
 
