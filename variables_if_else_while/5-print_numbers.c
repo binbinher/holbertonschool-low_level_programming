bd8bd6362a81a0fd654c9bdb8b25e73ae73e431d
binbinher <159251452+binbinher@users.noreply.github.com>
2024-02-20 15:02:15 +0100

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints all single digit numbers of base 10 starting from 0
  * Return: Always 0.
@@ -7,10 +6,8 @@
 int main(void)
 {
 	int num;
-	
 	for (num = 0; num < 10; num++)
-		printf("%d", num);
+	printf("%d", num);
 	printf("\n");
-
 	return (0);
 }
