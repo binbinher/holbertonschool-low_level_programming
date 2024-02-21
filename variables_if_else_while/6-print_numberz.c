25f6601a8f205c729471cf7ef0ebcad68e2a3893
binbinher <159251452+binbinher@users.noreply.github.com>
2024-02-21 10:40:06 +0100

    Update 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -5,10 +5,10 @@
 * only use putchar function no printf
 * Return: Always 0.
 */
-int main (void)
+int main(void)
 {
 	int num;
-	for (num = 0; num < 10; num++)
+	for(num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 	putchar('\n');
 	return (0);
