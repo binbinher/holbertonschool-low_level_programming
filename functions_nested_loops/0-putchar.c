f14262f74394eb997bdc17c8d4e862771911af6c
binbinher <159251452+binbinher@users.noreply.github.com>
2024-02-21 16:10:58 +0100

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include "main.h"
-
 /**
  * main - Prints _putchar followed by a new line.
  * Return: Always 0.
  */
-
 int main(void)
-	
 {
-        _putchar('_');
+	_putchar('_');
         _putchar('p');
         _putchar('u');
         _putchar('t');
@@ -17,7 +14,5 @@ int main(void)
         _putchar('a');
         _putchar('r');
         _putchar('\n');
-
         return (0);
 }
-
