5b845cb3327ed63070ad4659d41d119847b933f5
binbinher <159251452+binbinher@users.noreply.github.com>
2024-02-21 10:35:20 +0100

    Update 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - prints all single digit numbers of base 10 starting from 0, only use putchar function,no printf
+ * Return: Always 0.
+ */
 int main(void)
-{
-   /** main - prints all single digit numbers of base 10 starting from 0,
-   *		only use putchar function,no printf
-   */
-  	int num;
-	
+{	
+	int num;
 	for(num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	
 	putchar('\n');
-   
-   return 0;
+	return 0;
 }
