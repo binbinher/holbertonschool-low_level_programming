f5635e5b3fca78d76a09c9e4d2035416cf9b9cae
binbinher <159251452+binbinher@users.noreply.github.com>
2024-02-22 10:37:43 +0100

    Update 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints 10 times the alphabet in lowercase,followed by a new line.
- * Return: Always 0.
- */
+* print_alphabet - Prints 10 times the alphabet in lowercase
+*followed by a new line.
+* Return: Always 0.
+*/
 
 void print_alphabet_x10(void)
 {
   char letter;
   int i = 0;
-
+  
   while (i <= 9)
   {
-      for (letter = 'a'; letter <= 'z'; letter++)
-      {
-          _putchar(letter);
-      }
-      _putchar('\n');
-  
-      i++;
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+      _putchar(letter);
+    }
+    _putchar('\n');
+    i++;
   }
 }
