fb8d1eb081d5e3a3a4d414c367c72f3860cff910
binbinher <159251452+binbinher@users.noreply.github.com>
2024-02-22 11:01:34 +0100

    Update 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,8 +12,8 @@ void print_alphabet_x10(void)
   int i = 0;
   
   while (i <= 9)
-{
-  for (letter = 'a'; letter <= 'z'; letter++)
+  {
+    for (letter = 'a'; letter <= 'z'; letter++)
   {
     _putchar(letter);
   }
