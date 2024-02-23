8db6af4fe2dc0f5d06ff80f76eff94445915aa77
binbinher <159251452+binbinher@users.noreply.github.com>
2024-02-23 09:07:39 +0100

    Update 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-  *checks for lowercase character
-  *returns 1 if c is lowercase
-  *returns 0 otherwise
-  */
+ *checks for lowercase character
+ *returns 1 if c is lowercase
+ *returns 0 otherwise
+ */
 int _islower(int c)
 {
-  return (c >= 'a' && c <= 'z');
+ return (c >= 'a' && c <= 'z');
 }
