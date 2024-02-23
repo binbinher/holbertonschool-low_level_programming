b47c005ec5cb4c097f0397e49e1eba1717deee8c
binbinher <159251452+binbinher@users.noreply.github.com>
2024-02-23 09:53:41 +0100

    Update 5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,25 +1,22 @@
 #include "main.h"
 /**
   * print_sign -prints the sign of a number
-  *@n: the int to check
+  * @n: the int to check
   * Return: 1 and if n is > zero, 0 if n is zero, -1 if n is < zero
   */
-int print_sign(int n);
+int print_sign(int n)
 {
-  if( n > 0)
-  {
-    _putchar('+');
-    return (1);
-  }
-  else if ( n == 0)
-  {
-    _putchar(48);
-    return (0);
-  }
-  else if( n < 0)
-  {
-    _putchar('-');
-  }
-  return (-1);
-  
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
