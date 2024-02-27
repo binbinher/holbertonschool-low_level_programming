ee24a2fa50c605e0e73a56990fde9a2e162dc471
Lilola Her <binbin.he515@outlook.com>
2024-02-27 11:33:03 +0100

    update 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,32 +7,34 @@
  * and for the multiples of five prints Buzz
  * Return: Always 0 (Success)
  */
-
-int main()
-
+int main(void)
 {
 	int num;
+
 	for (num = 1; num <= 100; num++)
-	
-	if(num % 3 == 0 && num % 5 != 0)
 	{
-		printf(" Fizz");
+		if(num % 3 == 0 && num % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (num % 5 == 0 && num % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (num == 1)
+		{
+			printf("%d", num);
+		}
+		else
+		{
+			printf(" %d", num);
+		}	
 	}
-	else if (num % 5 == 0 && num % 3 != 0)
-	{
-		printf(" Buzz");
-	}
-	else if (num % 3 == 0 && num % 5 == 0)
-	{
-		printf(" FizzBuzz");
-	}
-	else if (num == 1)
-	{
-		printf("%d", num);
-	}
-	else
-	{
-		printf(" %d", num);
-	}
+	printf("\n");
+
 	return 0;
 }
