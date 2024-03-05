48cab41f05bc8a9df2e85082136dc593e8a0469d
Lilola Her <binbin.he515@outlook.com>
2024-03-05 12:26:56 +0100

    update style

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,15 @@
  */
 
 char *_strpbrk(char *s, char *accept) {
-    while (*s) {
-        char *p = accept;
-        while (*p) {
-            if (*s == *p) {
-                return s;
-            }
-            p++;
-        }
-        s++;
-    }
-    return NULL;
+ 	while (*s) {
+	char *p = accept;
+	while (*p) {
+		 if (*s == *p) {
+		 return s;
+		 }
+	 p++;
+	 }
+	 s++;
+	 }
+	 return NULL;
 }
