66486fb1a33b7750378d7db16d08a576182718b2
Lilola Her <binbin.he515@outlook.com>
2024-03-05 12:31:07 +0100

    update tsk4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -9,17 +9,16 @@
  * Return: pointer to the byte in s that matches one of the bytes in accept
  * or NULL if no such byte is found
  */
-
 char *_strpbrk(char *s, char *accept) {
- 	while (*s) {
-	char *p = accept;
+	while (*s) {
+  char *p = accept;
 	while (*p) {
-		 if (*s == *p) {
-		 return s;
-		 }
-	 p++;
-	 }
-	 s++;
-	 }
-	 return NULL;
+		if (*s == *p) {
+			return s;
+			}
+			p++;
+			}
+			s++;
+			}
+			return NULL;
 }
