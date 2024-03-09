51066b54ee0a097fe428b1b281d9589ff51fac50
Binchat <binbin.he515@outlook.com>
2024-03-09 09:18:58 +0100

    update tsk0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-#include "main.h"
 
 /**
  * main - prints the name of the program
- * @argc :number of arguments
+ * @argc: number of arguments
  * @argv: array of arguments
  * 
- *  Return: Always 0 
- * /
+ * Return: Always 0 
+ */
 
-int main(int argc _attribute_((unused)), char *argv[])
-	{
-		printf("%s\n", *argv);
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
 
-		return (0);
-	}
+	return (0);
+}
