ff04d3c075a835fd0457bdcf0f00d8950b2bca84
Binchat <binbin.he515@outlook.com>
2024-06-17 22:50:18 +0200

    update integer format to percentage i instead of percentage d

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 	{
 		printf("%i is positive\n ", n);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{	
-		printf("%i is negetive\n", n);
+		printf("%i is zero\n", n);
 	}
 	else
 	{
-		printf("%i is zero\n", n);}
+		printf("%i is negetive\n", n);}
 	
 	return (0);
 }
