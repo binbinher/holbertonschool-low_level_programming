db6073de3ca89d20f542d7ed8f9402e3963444f1
Lilola Her <binbin.he515@outlook.com>
2024-06-18 12:38:47 +0200

    fix betty style tsk4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main - prints the alphabet except q and e in lowercase, followed by a new line.
- * use putchar
+ * main - prints the alphabet except q and e in lowercase,
+ * followed by a new line, use putchar
  * Return: Always 0.
  */
 int main(void)
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z'; letter ++)
-	if (letter !='q' && letter != 'e')
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	if (letter != 'q' && letter != 'e')
 		putchar(letter);
 	putchar('\n');
 	return (0);
