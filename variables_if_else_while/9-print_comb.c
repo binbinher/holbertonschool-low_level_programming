482a479ce14003ea12c6f0837ef8b4da546fc69d
Binchat <binbin.he515@outlook.com>
2024-06-18 18:13:41 +0200

    add tsk9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * print in ascending order, use putchar only, 4 times max
+ * char is not allowed
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{	
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(',');
+			putchar('\n');
+		}
+	}
+	return (0);
+}
