0d130453f49f507d4b9131574e3cee08ef5c0575
Lilola Her <binbin.he515@outlook.com>
2024-06-18 12:22:41 +0200

    add tsk3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase,then uppercase,
+ * followed by a new line, use putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+                putchar (letter);
+        putchar('\n');
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar (letter);
+	putchar('\n');
+
+        return (0);
+}
