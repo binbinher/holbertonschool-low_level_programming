fd262b7c994dceeef2c8846628f4c50e5b990c94
Lilola Her <binbin.he515@outlook.com>
2024-06-18 13:06:24 +0200

    add tsk 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#includ <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * not using char as variable, use putchar 
+ * Return: Always 0. (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num =0; num < 10; num++)
+		putchar((num % 10) + '0');
+// '0' in ASCII value is 48,num % 10 print a single digit as a char in c
+	putchar('\n');	
+	return (0);
+}
