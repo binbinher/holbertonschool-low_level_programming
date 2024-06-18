a8e681d8063ed469796c30c912ee660d0e7d0e9d
Lilola Her <binbin.he515@outlook.com>
2024-06-18 12:06:01 +0200

    add tsk2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main: prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+	for(letter = 'a', letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	Return (0);
+}
