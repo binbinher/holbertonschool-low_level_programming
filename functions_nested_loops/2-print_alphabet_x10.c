c22d81c7ea809c09b8578b8f043975e425ebb2ab
Binchat <binbin.he515@outlook.com>
2024-06-19 17:06:49 +0200

    update using for only

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
- * use _putchar, prints 10 times
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * in lowercase, followed by a new line.
+ * use _putchar
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	int count = 0;
+	int count;
 	char letter;
 
-	while (count++ <= 9)
+	for (count = 0; count < 10; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
