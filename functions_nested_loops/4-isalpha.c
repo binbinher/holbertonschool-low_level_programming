51e6efb651ecb9e781725d5e3bc0389920cf5e6a
Binchat <binbin.he515@outlook.com>
2024-06-19 17:30:24 +0200

    misused for instead of if else

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
- * @c the letter to be checked
+ * @c: letter to be checked
  * Return: 1 if c is a letter,both lower and uppercase, return 0 otherwise
  */
 int _isalpha(int c)
 {
-	for ((c >= 'a'; c <= 'z') || (c >= 'A'; c <= 'Z'))
+	if ((c >= 'a'; c <= 'z') ||
+			(c >= 'A'; c <= 'Z'))
 		return (1);
-	else return (0);
+	else
+		return (0);
 }
