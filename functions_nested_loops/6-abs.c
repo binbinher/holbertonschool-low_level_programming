7d03ee4d25654146acd04a24d1a1945c3726cf29
Binchat <binbin.he515@outlook.com>
2024-06-19 18:25:49 +0200

    add tsk6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n:the integer to compute
+ * @r: the absolute value
+ * Return: Always 0.
+ */
+int _abs(int n, r)
+{
+	if (n >= 0)
+		r = n if (n < 0)
+				r = -n return (0);
+}
