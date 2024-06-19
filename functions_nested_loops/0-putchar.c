824a3cb9a9ecef7f84b65d520ca0ce3e435791a9
Binchat <binbin.he515@outlook.com>
2024-06-19 11:20:58 +0200

    add tsk0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - prints _putchar, followed by a new line
+ * 
+ * Return: Always 0. (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
