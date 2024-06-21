a6dae7fe8cdf718c379ed9627a579a3d444116c7
Binchat <binbin.he515@outlook.com>
2024-06-21 11:28:09 +0200

    fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,18 @@
  * Numbers must be separated by a comma, followed by a space
  * be printed in order
  */
-void print_to_98(int n) if (n >= 98)
+void print_to_98(int n)
 {
-	while (n > 98)
-		printf("%d, ", n--);
-	printf("%d\n", n);
-}
-else
-{
-	while (n < 98)
-		printf("%d, ", n++);
-	printf("%d\n", n);
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
