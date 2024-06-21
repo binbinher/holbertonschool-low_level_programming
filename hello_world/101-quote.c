9896635b28eb4fe5d4ccace2023cf62b8997eee1
Binchat <binbin.he515@outlook.com>
2024-06-21 12:11:28 +0200

    fix tsk9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,10 +7,6 @@
  */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\"
-										- Dora Korpar,
-						 2015 - 10 - 19 ";
-
-								 write(2, msg, 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
