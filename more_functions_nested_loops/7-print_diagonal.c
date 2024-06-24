fd6a7375bbff379d82ecbecf51bbcd54193793b2
Binchat <binbin.he515@outlook.com>
2024-06-24 22:23:10 +0200

    typo fix tsk7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,22 +8,19 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
+	int len, space;
 
 	if (n > 0)
 	{
-		for (i = 1; i < n; i++)
-			for (j = 1; j < i; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				_putchar('\n');
-				else _putchar(' ');
-			}
-		_putchar('\n');
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+			_putchar('\\');
+			if (len == (n - 1);
+			continue;
+			_putchar('\n');
+		}
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
