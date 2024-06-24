33422786f84d558b9e2b811c1ee9878109566da7
Lilola Her <binbin.he515@outlook.com>
2024-06-24 10:45:35 +0200

    add tsk2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a:the integer,@b: the integer to be multiplied
+ * Return: the result of the multipication
+ */
+int mul(int a, int b)
+{
+	return (a *  b);
+}
