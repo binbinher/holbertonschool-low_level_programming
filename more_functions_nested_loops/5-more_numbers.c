299b95a93e13689d5da5b17c2b0618a56438bf26
Lilola Her <binbin.he515@outlook.com>
2024-06-24 11:20:03 +0200

    betty style

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,9 +6,9 @@
  */
 void more_numbers(void)
 {
-int num, count;
-
-for (count = 0; count <= 9; count++)
+	int num, count;
+	
+	for (count = 0; count <= 9; count++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
