80d54b9506559c956d42361dabb89da22bc931f7
Lilola Her <binbin.he515@outlook.com>
2024-06-24 10:38:47 +0200

    add tsk1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ * @c:the integer to be checked
+ * Return: 1 if c is a digit; 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if( c >= 0 && c <= 9)
+		return (0);
+	else
+		return (1);
+}
+
