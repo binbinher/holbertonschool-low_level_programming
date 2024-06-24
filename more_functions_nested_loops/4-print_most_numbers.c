c53e962f3ac885f94446613bad185647bd60e87b
Lilola Her <binbin.he515@outlook.com>
2024-06-24 11:01:11 +0200

    add tsk4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * do not print 2 and 4
+ * _putchar only, twice max
+ */
+void print_most_numbers(void)
+{
+	int num;
+	
+	for (num >=n 0; num <= 9; num++)
+			|| (num != 2 && num != 4)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
