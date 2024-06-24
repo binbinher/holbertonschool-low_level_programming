551c9279b85bd37c46c3915d8de4d1e953935fc5
Lilola Her <binbin.he515@outlook.com>
2024-06-24 11:05:29 +0200

    betty style

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers - prints the numbers, 
+ * from 0 to 9, followed by a new line.
  * do not print 2 and 4
  * _putchar only, twice max
  */
 void print_most_numbers(void)
 {
 	int num;
-	
+
 	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
