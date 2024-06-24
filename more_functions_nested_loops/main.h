839eaaef02e7a9fbdf29c6de50513eb14a4e6536
Lilola Her <binbin.he515@outlook.com>
2024-06-24 09:53:16 +0200

    add mainh

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+/**
+ * this file includes the prototyps of all the functions
+ * for the 4th project: C - More functions, more nested loops
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
