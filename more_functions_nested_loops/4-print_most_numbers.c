bc738012b736b1652f92039ade485ff6b7e3d613
Lilola Her <binbin.he515@outlook.com>
2024-06-24 11:05:55 +0200

    betty style

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - prints the numbers, 
+ * print_most_numbers - prints the numbers
  * from 0 to 9, followed by a new line.
  * do not print 2 and 4
  * _putchar only, twice max
