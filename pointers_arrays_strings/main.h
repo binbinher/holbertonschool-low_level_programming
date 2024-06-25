20ff9db75f3196e94d912dd55119a91ca3bcbb74
Lilola Her <binbin.he515@outlook.com>
2024-06-25 13:22:21 +0200

    update putchar

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
  * this file includes the prototyps of all the functions
  * for the 4th week project C Pointers, arrays and strings
  */
-
+void _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
