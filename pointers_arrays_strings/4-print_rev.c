5afb04dddc550f0994b28e04da372daa45452992
Binchat <binbin.he515@outlook.com>
2024-06-25 19:49:32 +0200

    add strlen explanation from tsk2

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -15,3 +15,17 @@ void print_rev(char *s)
 
 	_putchar('\n');
 }
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: an input string
+ * Return: the lenth of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
