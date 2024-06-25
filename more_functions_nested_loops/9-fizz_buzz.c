24be36a16f5202f29ab8c9d64a0c411d0996ff07
Lilola Her <binbin.he515@outlook.com>
2024-06-25 10:44:59 +0200

    fix space

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,20 +14,21 @@ int main(void)
 		{
 			if (num % 3 == 0)
 			{	
-				printf("Fizz ");
+				printf("Fizz");
 			}
 			else if (num % 5 ==0)
 			{
-				printf("Buzz ");
+				printf("Buzz");
 			}
 			else if (num % 3 ==0 && num % 5 == 0)
 			{
-				printf("FizzBuzz ");
+				printf("FizzBuzz");
 			}
 			else 
 			{
 				printf("%d ",num);
 			}
+			printf(" ");
 		}
 	return(0);
 }
