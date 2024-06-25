a15877b419fbf53fe53c81f8ddefe7b992e59bf5
Lilola Her <binbin.he515@outlook.com>
2024-06-25 11:11:27 +0200

    fix betty

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,32 +6,24 @@
  * multiples of both three and five print FizzBuzz.
  * Return: Always 0.
  */
-int main( )
+int main(void)
 {
 	int num;
-	
+
 	for (num = 1; num <= 100; num++)
-		{
-			if ((num % 3) == 0 && (num % 5) == 0)
-			{
-				printf("FizzBuzz");
-			}
-			else if ((num % 3) == 0)
-			{
-				printf("Fizz");
-			}
-			else if ((num % 5) == 0)
-			{
-				printf("Buzz");
-			}
-			else
-			{
-				printf("%d", num);
-			}
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+		else if ((num % 3) == 0)
+			printf("Fizz");
+		else if ((num % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
 			if (num == 100)
 				continue;
-			printf(" ");
-		}
+			printf(" ");	
+	}
 	printf("\n");
 
 	return (0);
