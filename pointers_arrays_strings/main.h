3e265ddaf494ee0f015dbc12ae11b4423c673db1
Lilola Her <binbin.he515@outlook.com>
2024-06-25 12:17:48 +0200

    add mainh

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+/**
+ * this file includes the prototyps of all the functions
+ * for the 4th week project C Pointers, arrays and strings
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n); 
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
