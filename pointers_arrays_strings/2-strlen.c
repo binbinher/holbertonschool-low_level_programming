d38944ab310838238390c886b79e41198600a375
Lilola Her <binbin.he515@outlook.com>
2024-06-25 13:11:22 +0200

    betty

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,10 +2,12 @@
 /**
  * _strlen - returns the length of a string.
  * @s: an input string
+ * Return: the lenth of the string
  */
 int _strlen(char *s)
 {
 	int len = 0;
+
 	while (s[len] != '\0')
 		len++;
 	return (len);
