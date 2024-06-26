9ddbf1eaaefac194e7ae869d1f3d86f41a85ee01
Binchat <binbin.he515@outlook.com>
2024-06-26 18:55:00 +0200

    update tsk10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,14 +13,19 @@ int _atoi(char *s)
 	int sign = 1;
 	int i = 0;
 
-	if (s[0] == '-')
+	if (s[i] == '-' || s[i] == '++')
 	{
-		sign = -1;
+		if (s[0] == '-')
+		{
+			sign = -1;
+		}
 		i++;
 	}
 
-	for (; s[i] != '\0'; ++i)
+	while (s[i] >= '0' && s[i] <= '9')
+	{
 		result = result * 10 + s[i] - '0';
-
+		i++;
+	}
 	return (sign * result);
 }
