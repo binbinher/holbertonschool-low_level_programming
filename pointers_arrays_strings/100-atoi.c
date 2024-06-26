0e69f876a5ff6143ef8edc6e21df3b2d230e761b
Binchat <binbin.he515@outlook.com>
2024-06-26 18:42:43 +0200

    add tsk 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _atoi -  convert a string to an integer
+ * @s: the string to be converted
+ * the number in the string can be  preceded by
+ * an infinite number of characters
+ * be careful with positive and negative sign before the number
+ * Return: the converted int
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+
+	for (; s[i] != '\0'; ++i)
+		result = result * 10 + s[i] - '0';
+
+	return (sign * result);
+}
