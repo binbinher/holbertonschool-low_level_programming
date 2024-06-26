8c8bd78255db46059c1b4c1c644234c1e7e0c62f
Lilola Her <binbin.he515@outlook.com>
2024-06-26 12:33:57 +0200

    betty

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ void rev_string(char *s)
 
 	while (s[index++])
 		len++;
-	for (index = len - 1; index >= len / 2; index --)
+	for (index = len - 1; index >= len / 2; index--)
 	{
 		temp = s[index];
-		s[index] =s[len - index -1];
-		s[len -index -1] = temp;
+		s[index] =s[len - index-1];
+		s[len - index-1] = temp;
 	}
 }
