9ba152b069882bc87760f2b770106503da0e78ff
Binchat <binbin.he515@outlook.com>
2024-06-26 19:16:36 +0200

    betty

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,15 +5,15 @@
  * @src:the source string to be appened to @dest
  * Return: dest: a pointer to the resulting string dest
  */
- char *_strcat(char *dest, const char *src)
- {
+char *_strcat(char *dest, const char *src)
+{
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
-		
+
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
-	
+
 	return (dest);
- }
+}
