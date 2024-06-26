b92f557b03f8d594cf28aba98ae6935c14145525
Lilola Her <binbin.he515@outlook.com>
2024-06-26 13:23:28 +0200

    add tsk9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src, 
+ * including the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: a buffer to copy the string to
+ * @src: the source string to copy
+ * Return: *dest, a pointer to the string dest
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
