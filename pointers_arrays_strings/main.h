6f2f3f54d1cc3daafef2371d4bc9d197d9f273b9
Binchat <binbin.he515@outlook.com>
2024-06-26 18:48:23 +0200

    update mainh

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,12 +11,12 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n); 
+void print_array(int *a, int n);
 char *_strcpy(char *dest, const char *src);
 int _atoi(char *s);
-char *_strcat(char *dest, const char *src)
-char *_strncat(char *dest, char *src, int n)
-int _strcmp(char *s1, char *s2)
-void reverse_array(int *a, int n)
-char *string_toupper(char *)
-char *cap_string(char *)
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
