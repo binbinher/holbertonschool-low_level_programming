d761fc1bdf426173f9d8a2d499bf34b3cbc29748
Binchat <binbin.he515@outlook.com>
2024-06-30 19:24:58 +0200

    betty

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,9 @@
  * @s1:the first string
  * @s2:the second string
  * Return:
- * 				returns zero if s1 == s2
- * 				returns negative number if s1 < s2
- * 				returns positive number if s1 > s2
+ * returns zero if s1 == s2
+ * returns negative number if s1 < s2
+ * returns positive number if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
