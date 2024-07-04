d4d2d8d2317b321f717340fc65d62b3e47192fe2
Binchat <binbin.he515@outlook.com>
2024-07-05 00:00:16 +0200

    add tsk2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives.
+ * @argc: counts of arguments
+ * @argv: array of arguments
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0, i < argc, i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
