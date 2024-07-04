711fdb47bb774bbb20edd3bd69c4dfcd265d429b
Binchat <binbin.he515@outlook.com>
2024-07-04 16:00:42 +0200

    update mainh

--- recursion/main.h
+++ recursion/main.h
@@ -1,14 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <unistd.h>
+#ifndef MAIN
+#define MAIN
 
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int test_produit(int n, int p);
 int is_prime_number(int n);
+int test_diviseur(int n, int d);
 
 #endif
