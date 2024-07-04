ab09bfb92105844d37718128b5a8b2696c08a138
Binchat <binbin.he515@outlook.com>
2024-07-04 20:48:01 +0200

    add tsk6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * otherwise return 0.
+ * @n:the input integer
+ */
+int is_prime_number(int n)
+{
+	int stt = n / 2;
+
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, stt));
+}
+
+/**
+ * is_prime -return 1 if n is prime.
+ * @n: number to be checked
+ * @stt:the start number
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int is_prime(int n, int stt)
+{
+	if (stt <= 1)
+		return (1);
+	else if (n % stt == 0)
+		return (0);
+	return (is_prime(n, stt - 1));
+}
