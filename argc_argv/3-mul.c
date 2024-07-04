591d472cc251892befca557291ddd08b612964f5
Binchat <binbin.he515@outlook.com>
2024-07-05 00:10:23 +0200

    add tsk3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ * print the result of the multiplication, followed by a new line
+ * print Error if it does not receive two arguments
+ * Return 0 if success, otherwise 1
+ */
+int main(int argc, char *argv)
+{
+	int i, j, mul;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	mul = i * j;
+
+	printf("%d\n", mul);
+	return (0);
+}
