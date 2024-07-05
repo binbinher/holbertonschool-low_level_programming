0b87a65894f39c7a04937254e370cc0f797218ec
Binchat <binbin.he515@outlook.com>
2024-07-05 23:07:47 +0200

    fix tsk8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -3,28 +3,24 @@
 /**
  * print_diagsums - prints the sum of the two diagonals of
  * a square matrix of integers.
- * @a:
- * @size:
+ * @a: square matrix of which we print the sum of diagonals
+ * @size:the size of the matrix
  * Return: 0 if success
  */
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
-	int j = (size * size) - 1;
-	int diagdown = 0;
-	int diagup = 0;
+	int i;
 
-	while (i < size * size)
+	unsigned int sum, sum1;
+
+	sum = 0;
+	sum1 = 0;
+
+	for (i = 0; i < size; i++)
 	{
-		diagdown = diagdown + a[i];
-		i = i + (size + 1);
+		sum += a[(size * i) + i];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
 	}
 
-	while (j > size - 1)
-	{
-		j = j - (size - 1);
-		diagup = diagup + a[j];
-	}
-
-	printf("%i, %i\n", diagdown, diagup);
+	printf("%d, %d\n", sum, sum1);
 }
