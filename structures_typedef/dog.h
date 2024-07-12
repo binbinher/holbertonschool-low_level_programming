da48281c6e419f63e636629046cb12158e58decd
Binchat <binbin.he515@outlook.com>
2024-07-12 23:39:43 +0200

    update tsk0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,13 +2,13 @@
 #define MAIN
 
 /**
- * struct dog - structure of dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of the dog
- * Description: datas of a dog name age owner
+ *struct dog - structure of dog
+ *@name: name of dog
+ *@age: age of the dog
+ *@owner: dog's owner name
+ *
+ *Description: datas of a dog name age owner
  */
-
 typedef struct dog
 {
 	char *name;
@@ -19,8 +19,8 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 void _strcpy(char *dest, char *src);
 int _strlen(char *s);
+void free_dog(dog_t *d);
 
 #endif
