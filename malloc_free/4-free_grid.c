69356ae7897d98deef014061799a1d884d5b9b77
Binchat <binbin.he515@outlook.com>
2024-07-12 17:43:45 +0200

    documentation

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 /**
  * free_grid - frees the memory allocate for the grid.
- * @grid:
- * @height:
+ * @grid:grid to free
+ * @height: height of the grid
  * Return: nothing
  */
 void free_grid(int **grid, int height)
