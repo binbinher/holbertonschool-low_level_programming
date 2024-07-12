4eb0c50b57d7d68aa94cc1ceafc00efe7b9cb9c3
Binchat <binbin.he515@outlook.com>
2024-07-12 22:03:51 +0200

    fix tsk2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,13 +1,13 @@
 #include "main.h"
-include<stdlib.h>
-		/**
-		 * _calloc - allocates memory for an array, using malloc.
-		 * @nmemb: the number of elements to allocate
-		 * @size: the size of each element.
-		 * the memory is set to zero
-		 * Return:the allocated memory, or NULL when size is 0 or malloc fails
-		 */
-		void *_calloc(unsigned int nmemb, unsigned int size);
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: the number of elements to allocate
+ * @size: the size of each element.
+ * the memory is set to zero
+ * Return:the allocated memory, or NULL when size is 0 or malloc fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size);
 {
 	char *ptr;
 	unsigned int i;
