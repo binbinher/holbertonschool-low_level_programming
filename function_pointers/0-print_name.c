134ccb1e503f718b43404a6a5881bb615fb62f80
Binchat <binbin.he515@outlook.com>
2024-07-18 10:10:44 +0200

    fix tsk0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,7 +2,8 @@
 /**
  * print_name - prints a name.
  * @name: name to print
- * @f:function to use
+ * @f:a pointer to function to use
+ * Return: the function or otherwise nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
