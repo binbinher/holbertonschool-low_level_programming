97a279e822b3327a313eb7b36c4b92536333c53b
Binchat <binbin.he515@outlook.com>
2024-07-19 22:02:53 +0200

    add tsk0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - a function that returns the sum of all its parameters.
+ * @n: the constant number to be add.
+ * Return: to the sum, or 0 if n ==0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
